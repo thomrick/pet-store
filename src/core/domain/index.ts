49f233099eb07c77c730f5dc99c9f41a3b6927e5
Thomas Ricart <thomasr2309@gmail.com>
2019-07-26 17:16:52 +0200

    refactor(core) : remove unused handlers

--- src/core/domain/index.ts
+++ src/core/domain/index.ts
@@ -1,7 +1,5 @@
-export * from './adopt-cat.handler';
 export * from './aggregate';
 export * from './events';
 export * from './exceptions';
 export * from './model';
 export * from './ports';
-export * from './register-cat.handler';
