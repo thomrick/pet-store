c87d542f887c3316e81e41dfc937388ceffd39c8
Thomas Ricart <thomasr2309@gmail.com>
2019-07-26 13:09:36 +0200

    refactor(in-memory-query-bus) : remove unused test

--- src/bus/adapters/in-memory/in-memory.query-bus.spec.ts
+++ src/bus/adapters/in-memory/in-memory.query-bus.spec.ts
@@ -2,16 +2,6 @@ import { IQuery, IQueryBus, IQueryHandler, IQueryResult } from '../../../core';
 import { InMemoryQueryBus } from './in-memory.query-bus';
 
 describe('InMemoryQueryBus', () => {
-  it('can be created', () => {
-    const handlers: IQueryHandler[] = [
-      {
-        handle: jest.fn(),
-        subscribe: jest.fn(),
-      },
-    ];
-    const bus: IQueryBus = new InMemoryQueryBus(handlers);
-  });
-
   it('should ask query to the dedicated handler', () => {
     const data: any = { message: 'hello' };
     const query: IQuery = {
